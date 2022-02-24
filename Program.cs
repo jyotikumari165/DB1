6b70157a37fc39e440c38ecb5ac1dd15b9fe535d
jyoti kumari <67068295+jyotikumari165@users.noreply.github.com>
2022-02-24 08:51:40 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, temp;
+
+            Console.WriteLine("Enter the number for num1:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the number for num2:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Before swapping num1 = {0} and num2 = {1}", num1, num2);
+
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+
+            Console.WriteLine("After swapping num1 = {0} and num2 = {1}", num1, num2);
+            Console.WriteLine("Press any key to exist");
+            Console.ReadKey();
+        }
+    }
+}
